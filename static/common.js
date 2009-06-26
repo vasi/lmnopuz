66a0afd9c82621e1dde7fd9325c0a256c8da616b
Dave Vasilevsky <djvasi@gmail.com>
2009-06-26 05:20:24 -0400

    notes work for .puz

--- static/common.js
+++ static/common.js
@@ -26,35 +26,6 @@ function smoothScroll(element, desiredScrollTop) {
   }
 }
 
-// from http://blog.firetree.net/2005/07/04/javascript-find-position/
-// written by Peter-Paul Koch & Alex Tingle
-function findPosX(obj) {
-  var curleft = 0;
-  if (obj.offsetParent) {
-    while(1) {
-      curleft += obj.offsetLeft;
-      if (!obj.offsetParent) break;
-      obj = obj.offsetParent;
-    }
-  } else if (obj.x) {
-    curleft += obj.x;
-  }
-  return curleft;
-};
-
-function findPosY(obj) {
-  var curtop = 0;
-  if (obj.offsetParent) {
-    while (1) {
-      curtop += obj.offsetTop;
-      if (!obj.offsetParent) break;
-      obj = obj.offsetParent;
-    }
-  } else if (obj.y) {
-    curtop += obj.y;
-  }
-  return curtop;
-};
 
 function toWidth(num, width) {
   var out = num.toString();
