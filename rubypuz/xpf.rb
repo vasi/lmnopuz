a105f1924c8ef6b17bc449f7a47d965d23e5c517
Dave Vasilevsky <dave@vasilevsky.ca>
2020-09-05 03:08:51 -0400

    allocate xpf2 clues

--- rubypuz/xpf.rb
+++ rubypuz/xpf.rb
@@ -41,17 +41,52 @@ class XPFCrossword < Crossword
 		
 		@down = {}
 		@across = {}
-		doc.search('Clues/Clue').each do |clue|
-			row, col, num, dir, ans = *%w[Row Col Num Dir Ans].
-				map { |k| clue[k] }
-			row, col, num = *[row, col, num].map { |a| a.to_i } 
-			dir = dir.downcase.to_sym
-			@squares[col - 1][row - 1].__send__("#{dir}=", num)
-			__send__(dir)[num] = clue.inner_text
-			# TODO: Do something with ans (rebus?)
+		if doc.name == "Puzzle"
+			doc.search('Clues/Clue').each do |clue|
+				row, col, num, dir, ans = *%w[Row Col Num Dir Ans].
+					map { |k| clue[k] }
+				row, col, num = *[row, col, num].map { |a| a.to_i }
+				dir = dir.downcase.to_sym
+				@squares[col - 1][row - 1].__send__("#{dir}=", num)
+				__send__(dir)[num] = clue.inner_text
+				# TODO: Do something with ans (rebus?)
+			end
+		else
+			assign_clues(doc)
 		end
 		
 		# TODO: Rebus, Notepad, Circle, Shade
 	end
-end
 
+	# Allows x, y to be outside range!
+	private def has_letter(x, y)
+		return false if x < 0 || y < 0
+		return false if x >= @width || y >= @height
+		!@squares[x][y].nil?
+	end
+
+	def assign_clues(doc)
+		clues_across = doc.search('Across/Clue')
+		clues_down = doc.search('Down/Clue')
+
+		clues_across.each do |clue|
+			@across[clue['Num'].to_i] = clue.inner_text
+		end
+		clues_down.each do |clue|
+			@down[clue['Num'].to_i] = clue.inner_text
+		end
+
+		# Allocate clue numbers. A square gets a number if it starts an answer,
+		# and doesn't end one.
+		num = 1
+		(0...@height).each do |y|
+			(0...@width).each do |x|
+				has_across = !has_letter(x - 1, y) && has_letter(x, y) && has_letter(x + 1, y)
+				@squares[x][y].across = num if has_across
+				has_down = !has_letter(x, y - 1) && has_letter(x, y) && has_letter(x, y + 1)
+				@squares[x][y].down = num if has_down
+				num += 1 if has_across || has_down
+			end
+		end
+	end
+end
